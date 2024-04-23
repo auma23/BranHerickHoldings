21d03a74c6aee3cccfd533dd1ea9de2d9fc8b48c
auma23 <dianaauma75@gmail.com>
2024-04-23 14:13:27 +0300

    add initial website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { BranNavbar } from './Components/BranNavbar'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <BranNavbar />
+    </h1>
+  )
+}
+
+export default App
