10830a79b18bf0618e5cefa4da59ded31ada40bd
auma23 <dianaauma75@gmail.com>
2024-04-25 15:34:42 +0300

    style services.jsx file

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,16 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLaptop, faUniversity, faLink, faServer } from '@fortawesome/free-solid-svg-icons';
+import { faLaptop, faUniversity, faGlobe, faUsers, faGraduationCap, faPlane } from '@fortawesome/free-solid-svg-icons';
 
 const FeatureCard = ({ icon, title, description }) => {
   return (
     <div className="w-full px-4 md:w-1/2 lg:w-1/3">
       <div className="mb-9 rounded-xl py-8 px-7 shadow-md transition-all hover:shadow-lg sm:p-9 lg:px-6 xl:px-9">
         <div className="mx-auto mb-7 inline-block">
-          <FontAwesomeIcon icon={icon} className="text-6xl text-purple-600" />
+          <FontAwesomeIcon icon={icon} className="text-6xl text-harvestGold" />
         </div>
         <div>
-          <h3 className="mb-4 text-xl font-bold text-black sm:text-2xl lg:text-xl xl:text-2xl">{title}</h3>
-          <p className="text-base font-medium text-body-color">{description}</p>
+          <h3 className="mb-4 text-xl font-bold text-licorice sm:text-2xl lg:text-xl xl:text-2xl">{title}</h3>
+          <p className="text-base font-medium text-licorice">{description}</p>
+          <button className="bg-harvestGold hover:bg-licorice text-cream font-bold py-2 px-4 rounded mt-4 transition-all duration-300 ease-in-out">Load More</button>
         </div>
       </div>
     </div>
@@ -20,7 +21,7 @@ const FeatureCard = ({ icon, title, description }) => {
 const Services = () => {
   return (
     <div className="p-8">
-    <h3 className="text-4xl font-bold mb-8">Our Services</h3>
+    <h3 className="text-4xl font-bold mb-8 text-harvestGold">Our Services</h3>
     <div className="-mx-4 flex flex-wrap p-8">
       <FeatureCard
         icon={faLaptop}
@@ -33,22 +34,22 @@ const Services = () => {
         description="Our team of well-trained and experienced international producers always deliver highly well-researched topics that addresses the real market"
       />
       <FeatureCard
-        icon={faServer}
+        icon={faUsers}
         title="Team Building"
         description="Strong, cohesive teams are the backbone of successful organizations. Our team-building experiences are designed to foster collaboration…"
       />
       <FeatureCard
-        icon={faLink}
+        icon={faGraduationCap}
         title="Training Programs"
         description="We offer a diverse range of training programs designed to enhance the skills and knowledge of your workforce."
       />
       <FeatureCard
-        icon={faLink}
+        icon={faPlane}
         title="Tours and Travel"
         description="Leveraging on both an international and local portfolio of tours and travel agents and solutions…."
       />
       <FeatureCard
-        icon={faLink}
+        icon={faGlobe}
         title="Conferences and Summits"
         description="Through a substantial presence in each industry and country across the continents, our conferences and summits leverage the latest….."
       />
